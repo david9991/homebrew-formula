f311761a7113bf5f35001c1aeec0d4ab934af80a
David <david@pocograph.com>
2016-10-09 13:28:39 +0800

    Upgrade libiec61850 to 0.9.3
    Change-Id: Ia007552af36f42d9c8cc2cc74fad9cb0351449b1

--- Formula/libiec61850.rb
+++ Formula/libiec61850.rb
@@ -1,15 +1,15 @@
 class Libiec61850 < Formula
-  version "0.9.2.1"
+  version "0.9.3"
   desc ""
   homepage ""
-  url "http://libiec61850.com/libiec61850/wp-content/uploads/2016/06/libiec61850-0.9.2.1.tgz"
+  url "http://libiec61850.com/libiec61850/wp-content/uploads/2016/10/libiec61850-0.9.3.tgz"
   sha256 ""
 
   depends_on "cmake" => :build
   depends_on "xz"
 
   def install
-    system "cmake", "-DCONFIG_MMS_THREADLESS_STACK=ON","-DBUILD_EXAMPLES=OFF","-DCMAKE_INSTALL_PREFIX=#{prefix}","."
+    system "cmake", "-DBUILD_EXAMPLES=OFF", "-DCMAKE_INSTALL_PREFIX=#{prefix}", "."
     system "make"
     system "make", "install"
   end
