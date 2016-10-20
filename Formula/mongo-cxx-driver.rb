4a1879cbe71f62376f9b6ddcd5000a2471a8ebf5
David <david@pocograph.com>
2016-10-20 10:13:58 +0800

    Remove useless dependencies.
    Change-Id: Icacb9ff4d895868d3427cf3182e40d9326ab7404

--- Formula/mongo-cxx-driver.rb
+++ Formula/mongo-cxx-driver.rb
@@ -5,10 +5,7 @@ class MongoCxxDriver < Formula
   url "https://github.com/mongodb/mongo-cxx-driver/archive/r3.0.1.tar.gz"
   sha256 ""
 
-  depends_on "cmake" => :build
-  depends_on "libbson"
   depends_on "mongo-c"
-  depends_on "xz"
   def install
     system "cmake", "-DCMAKE_INSTALL_PREFIX=#{prefix}", "."
     system "make", "install"
